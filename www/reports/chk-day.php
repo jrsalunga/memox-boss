5d544600637c5940de3a515707cd3c69a1c26fed
jrsalunga <jefferson.salunga@yahoo.com>
2014-06-03 11:52:30 +0800

    fixed like mfi

--- www/reports/chk-day.php
+++ www/reports/chk-day.php
@@ -14,6 +14,7 @@ if(isset($_GET['fr']) && isset($_GET['to'])){
 
 $uri = explode('?',$_SERVER['REQUEST_URI']);
 $qs = !empty($uri[1]) ? '?'.$uri[1] : '?fr='.$dr->fr.'&to='.$dr->to;
+
 ?>
 <!DOCTYPE HTML>
 <html lang="en-ph">
